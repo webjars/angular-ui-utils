7102d62414eee75413382b60189de78895a94f63
James Ward <james@jamesward.org>
2014-03-01 17:38:18 -0700

    updated requirejs support - fixes #2

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,8 +1,4 @@
-/*global requirejs */
-
-// Ensure any request for this webjar brings in dependencies.
 requirejs.config({
-    shim: {
-        'ui-utils': [ 'webjars!angular-ui.js', 'webjars!jquery.js' ]
-    }
+    paths: { "angular-ui-utils": webjars.path("angular-ui-utils", "ui-utils") },
+    shim: { "angular-ui-utils": [ "angular", "jquery" ] }
 });
